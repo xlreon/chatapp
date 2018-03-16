23965fbef6d0ab1007e8537ef852429cd6909c6a
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-03-16 18:25:19 +0530

    Added UI and logic for sending messages

--- lib/main.dart
+++ lib/main.dart
@@ -21,16 +21,35 @@ class ChatScreen extends StatefulWidget {
 
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _message = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
   @override 
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title : new Text("Chat App")
       ),
-      body : _buildTextComposer()
+      body : new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(0.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _message[index],
+              itemCount: _message.length,
+            ),
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(
+              color : Theme.of(context).cardColor
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
     );
   }
-  final TextEditingController _textController = new TextEditingController();
 
   Widget _buildTextComposer() {
     return new IconTheme(
@@ -61,5 +80,43 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message  = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _message.insert(0, message);
+    });
   }
 }
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin : const EdgeInsets.symmetric(vertical: 10.0),
+      child : new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin : const EdgeInsets.only(right : 16.0),
+            child : new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin : const EdgeInsets.only(top : 5.0),
+                child: new Text(text),
+              )
+            ],
+          )
+        ],
+      )
+    );
+  }
+}
+
+const String _name = "Sidharth";
